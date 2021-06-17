b8c82d88aae8e3ae145801f5059ba124c70915de
iamstephan <stephanburger54@gmail.com>
2021-06-17 18:50:41 +0200

    inline svg

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,33 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import twstyled from 'vite-plugin-twstyled'
+//@ts-ignore
+import svgLoader from 'vite-plugin-react-svg'
 
 export default defineConfig({
-  plugins: [twstyled(), reactRefresh()],
+  plugins: [
+    twstyled(),
+    reactRefresh(),
+    svgLoader({
+      defaultExport: 'component',
+    }),
+  ],
   base: './',
   root: resolve('./src/renderer'),
   build: {
     outDir: resolve('./dist'),
-    emptyOutDir: true
+    emptyOutDir: true,
   },
   resolve: {
     alias: [
       {
         find: '@/renderer',
-        replacement: resolve(__dirname, 'src/renderer')
+        replacement: resolve(__dirname, 'src/renderer'),
       },
       {
         find: '@/common',
-        replacement: resolve(__dirname, 'src/common')
-      }
-    ]
-  }
+        replacement: resolve(__dirname, 'src/common'),
+      },
+    ],
+  },
 })
